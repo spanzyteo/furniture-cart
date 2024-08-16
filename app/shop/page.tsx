f9a73b60a5ece9b09316a56983c2fb1f0c75ef98
spanzy teo <spanzytheo@gmail.com>
2024-08-16 11:14:59 +0100

    modification on the product page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -4,47 +4,46 @@ import Image from 'next/image'
 import { Parallax } from 'react-parallax'
 import { Product } from '../data/product'
 import { RiArrowRightSLine } from 'react-icons/ri'
+import bgProduct from '../../public/bg-product.jpg'
 
 const page: React.FC = () => {
   return (
     <>
       <div className="flex flex-col ">
-        <Parallax strength={800}>
-          <div className="flex items-center h-[230px] w-[100%] bg-cover bg-center bg-product-bg">
-            <h1 className="font-bold text-3xl text-white ml-20">
-              Our Products
-            </h1>
-          </div>
+        <Parallax
+          strength={300}
+          className="h-[230px] w-[100%] bg-cover bg-center flex items-center"
+          bgImage={bgProduct.src}
+        >
+          <h1 className="font-bold text-3xl text-white ml-20">Our Products</h1>
         </Parallax>
-        {/* <Parallax strength={800}> */}
-          <div className="mt-28 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-10">
-            {Product.map((item) => (
-              <div
-                key={item.id}
-                className="flex flex-col w-[240px] h-[380px] mx-auto rounded-xl shadow-custom"
-              >
-                <div className="w-[90%] h-[180px] mt-4 mx-auto">
-                  <Image
-                    src={item.image}
-                    alt="image"
-                    objectFit="cover"
-                    className="rounded-xl"
-                  />
-                </div>
-                <div className="flex flex-col mt-16 text-left ml-4">
-                  <h1 className="text-white font-semibold">{item.name}</h1>
-                  <h1 className="text-[#fab702] mt-2 font-semibold">
-                    ${item.price}
-                  </h1>
-                </div>
-                  <button className="mx-auto text-white font-semibold border mt-2 p-2 flex w-[90%] items-center justify-center relative hover:bg-[#fab702] hover:border-none group">
-                    <p>Add To Cart</p>
-                    <RiArrowRightSLine className="absolute text-[#fab702] right-6 h-[25px] w-[25px] font-bold transition-all duration-300 ease  group-hover:right-3 group-hover:text-black" />
-                  </button>
+        <div className="mt-28 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-10">
+          {Product.map((item) => (
+            <div
+              key={item.id}
+              className="flex flex-col justify-around w-[85%] lg:w-[240px] lg:h-[380px] h-[480px] mx-auto rounded-xl shadow-custom"
+            >
+              <div className="w-[90%] h-[180px] mt-[-1rem] mx-auto">
+                <Image
+                  src={item.image}
+                  alt="image"
+                  objectFit="cover"
+                  className="rounded-xl"
+                />
               </div>
-            ))}
-          </div>
-        {/* </Parallax> */}
+              <div className="flex flex-col text-left ml-4 mt-6 lg:mt-2">
+                <h1 className="text-white font-semibold">{item.name}</h1>
+                <h1 className="text-[#fab702] mt-2 font-semibold">
+                  ${item.price}
+                </h1>
+              </div>
+              <button className="mx-auto text-white font-semibold border sm:p-2 p-0 flex w-[90%] items-center justify-center relative hover:bg-[#fab702] hover:border-none group">
+                <p>Add To Cart</p>
+                <RiArrowRightSLine className="absolute text-[#fab702] right-6 h-[25px] w-[25px] font-bold transition-all duration-300 ease  group-hover:right-3 group-hover:text-black" />
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )

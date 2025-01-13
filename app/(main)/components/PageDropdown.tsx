10243fbc40a38388e0c0c790d6caa3ad1efc3b33
spanzy teo <spanzytheo@gmail.com>
2025-01-13 16:33:09 +0100

    recent modifications

--- app/(main)/components/PageDropdown.tsx
+++ app/(main)/components/PageDropdown.tsx
@@ -24,7 +24,9 @@ const PageDropdown = () => {
   return (
     <>
       <div className="w-[230px] group-hover:flex hidden absolute top-20 left-5 text-white bg-[#18191B] flex-col items-start  z-10 gap-2 p-4">
-        <h1 className="cursor-pointer hover:text-[#fab702]">Projects</h1>
+        <Link href={'/projects'}>
+          <h1 className="cursor-pointer hover:text-[#fab702]">Projects</h1>
+        </Link>
         <div className="h-[1px] w-full border-t border-t-[#333333]"></div>
         <Link href={'/training'}>
           <h1 className="cursor-pointer hover:text-[#fab702]">Training</h1>
@@ -42,7 +44,9 @@ const PageDropdown = () => {
             transition={{ duration: 0.5, ease: 'easeInOut' }}
             className="lg:hidden flex text-white bg-[#18191B] flex-col items-start pl-6 z-10 gap-5 mt-4 w-[100%]"
           >
-            <h1 className="cursor-pointer hover:text-[#fab702]">Projects</h1>        
+            <Link href={'/projects'} onClick={() => closeMenubar()}>
+              <h1 className="cursor-pointer hover:text-[#fab702]">Projects</h1>
+            </Link>
             <div className="h-[1px] w-full border-t border-t-[#333333]"></div>
             <Link href={'/training'} onClick={() => closeMenubar()}>
               <h1 className="cursor-pointer hover:text-[#fab702]">Training</h1>

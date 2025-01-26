9e30d232d0eaeb80d43bf2bce0375ed259eb7a66
spanzy teo <spanzytheo@gmail.com>
2025-01-26 11:38:22 +0100

    admin product page modification

--- app/(admin)/admin/products/page.tsx
+++ app/(admin)/admin/products/page.tsx
@@ -7,7 +7,7 @@ import { RiDeleteBin5Line } from 'react-icons/ri'
 const Products = () => {
   return (
     <div className="bg-white flex flex-col">
-      <div className="xl:ml-[20rem] mt-8 bg-[#F2F2F2] flex flex-col px-4 w-[90%] lg:w-[1014px] rounded-xl mx-auto mb-8">
+      <div className="xl:ml-[20rem] mt-8 bg-[#F2F2F2] flex flex-col px-4 w-[90%] lg:w-[1014px] rounded-xl mx-auto mb-8 pb-8">
         <div className="flex items-center justify-between mt-4">
           <h1 className="font-semibold sm:text-xl text-lg">Product List</h1>
           <button className="px-7 py-2 bg-[#fab702] rounded-[5px] text-white text-[13px] font-semibold hover:text-black hover:opacity-75 active:opacity-60 transition-all duration-500 ease-in-out">

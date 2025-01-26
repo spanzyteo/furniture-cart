c16b7bc61630ca800f8ac58e7e32e3d1b9f90838
spanzy teo <spanzytheo@gmail.com>
2025-01-26 22:16:44 +0100

    modify add-new-product page for admin

--- app/(admin)/admin/add-new-products/page.tsx
+++ app/(admin)/admin/add-new-products/page.tsx
@@ -1,7 +1,163 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
-const page = () => {
-  return <div>page</div>
+const AddNewProducts = () => {
+   const [fileName, setFileName] = useState('No file chosen')
+   const [thumbnailFilename, setThumbnailFilename] = useState('No file chosen')
+
+   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+     const file = event.target.files?.[0]
+     setFileName(file ? file.name : 'No file chosen')
+   }
+
+   const handleThumbnailFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+     const file = event.target.files?.[0]
+     setThumbnailFilename(file ? file.name : 'No file chosen')
+   }
+  return (
+    <div className="bg-white flex flex-col">
+      <form>
+        <div className="xl:ml-[27rem] mt-8 bg-[#F2F2F2] flex flex-col px-4 w-[90%] lg:w-[777px] rounded-xl mx-auto mb-8 pb-8">
+          <h1 className="text-xl font-semibold mt-4">Product Information</h1>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Product Name</h1>
+            <input
+              type="text"
+              placeholder="Product Name"
+              className="border border-[#EFEFEF] bg-[#F9F9F6] lg:w-[539px] w-full py-[10px] pl-3 focus:outline-none rounded-[5px] text-[#4A5568]"
+              required
+            />
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Category</h1>
+            <select
+              className="focus:outline-none border border-[#EFEFEF] bg-[#F9F9F6] lg:w-[539px] w-full py-[10px] rounded-[5px] text-[#4A5568] pl-3"
+              name="category"
+              id="category"
+              defaultValue="Bed"
+              title="category"
+              required
+            >
+              <option value="Bed" className="text-black font-semibold">
+                Bed
+              </option>
+              <option value="Cabinet" className="text-black font-semibold">
+                Cabinet
+              </option>
+              <option value="Chair" className="text-black font-semibold">
+                Chair
+              </option>
+              <option value="Desk" className="text-black font-semibold">
+                Desk
+              </option>
+              <option value="Sofa" className="text-black font-semibold">
+                Sofa
+              </option>
+              <option value="Misc" className="text-black font-semibold">
+                Misc
+              </option>
+            </select>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Price</h1>
+            <input
+              type="number"
+              placeholder="Price"
+              className="border border-[#EFEFEF] bg-[#F9F9F6] lg:w-[539px] w-full py-[10px] pl-3 focus:outline-none rounded-[5px] text-[#4A5568]"
+              required
+            />
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Stock</h1>
+            <input
+              type="number"
+              placeholder="Stock"
+              className="border border-[#EFEFEF] bg-[#F9F9F6] lg:w-[539px] w-full py-[10px] pl-3 focus:outline-none rounded-[5px] text-[#4A5568]"
+              required
+            />
+          </div>
+        </div>
+        <div className="xl:ml-[27rem] mt-8 bg-[#F2F2F2] flex flex-col px-4 w-[90%] lg:w-[777px] rounded-xl mx-auto mb-8 pb-8">
+          <h1 className="text-xl font-semibold mt-4">Product Images</h1>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Images</h1>
+            <div className="custom-file-input-wrapper">
+              {/* Hidden File Input */}
+              <input
+                type="file"
+                accept="image/*"
+                id="product-image"
+                onChange={handleFileChange}
+                className="hidden"
+                required
+              />
+
+              {/* Custom Label */}
+              <label
+                htmlFor="product-image"
+                className="custom-file-label border border-gray-200 bg-[#F9F9F6] lg:w-[539px] h-[40px] focus:outline-none rounded-[5px] text-[#4A5568] flex items-center cursor-pointer"
+              >
+                <span className="file-label-text bg-gray-200 h-[40px] px-3 text-black flex items-center">
+                  Choose File
+                </span>
+                <span className="file-name text-sm text-gray-500 ml-4">
+                  {fileName}
+                </span>
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">Thumbnail Image</h1>
+            <div className="custom-file-input-wrapper">
+              {/* Hidden File Input */}
+              <input
+                type="file"
+                accept="image/*"
+                id="thumbnail-image"
+                onChange={handleThumbnailFileChange}
+                className="hidden"
+                required
+              />
+
+              {/* Custom Label */}
+              <label
+                htmlFor="thumbnail-image"
+                className="custom-file-label border border-gray-200 bg-[#F9F9F6] lg:w-[539px] h-[40px] focus:outline-none rounded-[5px] text-[#4A5568] flex items-center cursor-pointer"
+              >
+                <span className="file-label-text bg-gray-200 h-[40px] px-3 text-black flex items-center">
+                  Choose File
+                </span>
+                <span className="file-name text-sm text-gray-500 ml-4">
+                  {fileName}
+                </span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="xl:ml-[27rem] mt-8 bg-[#F2F2F2] flex flex-col px-4 w-[90%] lg:w-[777px] rounded-xl mx-auto mb-8 pb-8">
+          <h1 className="text-xl font-semibold mt-4">Description</h1>
+          <div className="flex flex-col lg:flex-row lg:items-cente justify-between mt-4 gap-3 lg:gap-0">
+            <h1 className="font-semibold text-[#4A5568]">
+              Product Description
+            </h1>
+            <textarea
+              title="text"
+              placeholder="Description..."
+              rows={3}
+              className="border border-[#EFEFEF] bg-[#F9F9F6] lg:w-[539px] w-full py-[10px] pl-3 focus:outline-none rounded-[5px] text-[#4A5568]"
+              required
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-[#fab702] h-[40px] w-[140px] mx-auto text-white rounded-[5px] mb-10 text-[14px] font-semibold xl:ml-[27rem] hover:text-black hover:opacity-75 active:opacity-55 transition-all duration-500 ease-in-out"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
-export default page
+export default AddNewProducts
